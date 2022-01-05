b57456931ba3115eeda209989b107a1babdbe963
scosy <amine.bentaleb064@gmail.com>
2022-01-05 17:15:20 +0100

    Fixed errors in code.

--- eau04.rb
+++ eau04.rb
@@ -2,11 +2,16 @@
 
 # Fonctions utilisées
 def is_numeric_and_positive(str)
-  str.to_i < 0 ? false : true if !str.match?(/[a-zA-Z]/)
+  if str.to_i < 0 
+    false
+  else 
+    !str.match?(/[a-zA-Z]/) ? true : false
+  end
 end
 
 def is_prime_number(number)
-  if ((number.to_i != 2)&&(number.to_i == 1 || number.to_i % 2 == 0 || number.to_i % 3 == 0 || number.to_i % 5 == 0))
+  arr = [2, 3, 5]
+  if ((!arr.include?(number))&&(number.to_i == 1 || number.to_i % 2 == 0 || number.to_i % 3 == 0 || number.to_i % 5 == 0))
     false
   else
     true
@@ -14,15 +19,16 @@ def is_prime_number(number)
 end
 
 def prime_number(number)
-  number =  number.to_i + 1 while !is_prime_number(number); number
+  number = (number.to_i + 1) while !is_prime_number(number)
+  number
 end
 
 # Gestion d'erreur
-puts "-1" if ARGV.length != 1; exit
-puts "-1" unless is_numeric_and_positive(ARGV[0]); exit
+(puts "-1"; exit) if ARGV.length != 1
+(puts "-1"; exit) unless is_numeric_and_positive(ARGV[0])
 
 # Parsing
-number = ARGV[0].to_i
+number = ARGV[0].to_i + 1
 
 # Résolution
 next_prime_number = prime_number(number)
