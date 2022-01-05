8ed71333291e9c3550f9ff33dcf290fd32775c4a
scosy <amine.bentaleb064@gmail.com>
2022-01-05 16:45:57 +0100

    Fixed an error and optimised code.

--- eau12.rb
+++ eau12.rb
@@ -17,18 +17,19 @@ end
 def my_bubble_sort(array)
   range = (array.size - 1)..1
   (range.first).downto((range).last).each do |ind|
-    is_sorted = true
     for j in 0.. ind - 1
       (array[j + 1] < array[j]) ? (array.insert(j + 1, array.slice!(j))) : ()
-      is_sorted  
     end
   end
-  array
+  array.join(" ")
 end
 
 # Gestion d'erreurs
 (puts "error"; exit) if !only_numbers(ARGV)
 (puts "error"; exit) if ARGV.size < 2
 
+# Parsing
+number = ARGV.map! { |e| e.to_i }
+
 # Affichage
-puts my_bubble_sort(ARGV)
+puts my_bubble_sort(number)
